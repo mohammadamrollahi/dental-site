2cdb2cb654390d3bf07b4bf8a2e6709d53c7ed19
Mohammad amrollahi <52289677+mohammadamrollahi@users.noreply.github.com>
2020-03-02 22:59:19 +0330

    commit home page

---
+++ indexjs.js
@@ -0,0 +1,20 @@
+
+function esm()
+{
+
+	document.getElementById("nametitle").style.visibility="visible"
+
+}
+function esm2()
+{
+	document.getElementById("phontitle").style.transition=" 1s"
+
+	document.getElementById("phontitle").style.visibility="visible"
+
+}
+function esm3()
+{
+
+	document.getElementById("emailtitle").style.visibility="visible"
+
+}
